555f89d40008756ea596ad3c7f4fb4eb3f82d63b
hiagrawal <agrawal.hina13@gmail.com>
2021-10-11 19:32:21 +0530

    Using MongoDB to store sessions

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://MongoDbUser:MongoDbUser@cluster0.kij6e.mongodb.net/shopMongoDb?retryWrites=true&w=majority';
+
 const app = express();
+const store = MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+})
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -19,9 +26,11 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret:'my secret', resave: false, saveUninitialized: false}));
+app.use(session({secret:'my secret', resave: false, saveUninitialized: false, store: store}));
 //secret will be used for signing the hash which will be used to secretly store our id in the cookie
 //resave and saveUninitialized is used for performance that is do not save the session everytime till there is no change in the session value
+//store is used to define where it needs to store to which we have given mongodb
+//if not given anything, then by default stores in memory
 
 app.use((req, res, next) => {
   User.findById('6161733cd2ec6bef280e800f')
@@ -39,7 +48,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect('mongodb+srv://MongoDbUser:MongoDbUser@cluster0.kij6e.mongodb.net/shopMongoDb?retryWrites=true&w=majority')
+  .connect(MONGODB_URI)
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
